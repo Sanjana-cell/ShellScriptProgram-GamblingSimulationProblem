99253fdc42e18a7a512298a08e5248227e05c79e
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-30 15:30:15 +0530

    Refactor: added apporiate comments to the statements

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -10,15 +10,17 @@ stake_amount=$STAKE
 
 echo "WELCOME TO GAMBLER GAME"
 
-#Loops until total_amount_won and total_amount_lost is less then 50% of stake
+#Loops until stake amount less then 50% of stake
 while [ $stake_amount -gt 50 -a $stake_amount -lt 150 ]
 do
 	random=$(($RANDOM%2))
 	if [ $random -eq 1 ]
 	then
+		#if random is 1 stake amount is incremented by bet amount
 		stake_amount=$(($stake_amount+$betAmount))
 		echo "GAME WON BY GAMBLER"
 	else
+		#if random is 0 stake amount is decremented by bet amount  
 		stake_amount=$(($stake_amount-$betAmount))
 		echo "GAME LOST BY GAMBLER"
 	fi
