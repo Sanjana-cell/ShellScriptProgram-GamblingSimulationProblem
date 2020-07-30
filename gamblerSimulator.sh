a5f53de362594e3cfb79cab36171c35eb3c623a3
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-30 13:26:03 +0530

    Refactor: modified code by determining whether gambler as won or lost the game

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -5,7 +5,15 @@ STAKE=100
 
 #variables
 no_of_bets=0
+random=0
 
 ((no_of_bets++))
 echo "WELCOME TO GAMBLER GAME"
+random=$(($RANDOM%2))
+if [ $random -eq 1 ]
+then
+	echo "GAME WON BY GAMBLER"
+else
+	echo "GAME LOST BY GAMBLER"
+fi
 echo "Bet Amount = " $no_of_bets
