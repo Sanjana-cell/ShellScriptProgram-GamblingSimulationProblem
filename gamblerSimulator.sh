6c8ef0dbea58b14825aa625b86a09851f2e720c4
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-30 14:37:23 +0530

    Refactor: modified code to stop game for day when wining amount or lost amount is of 50% of stake

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -4,16 +4,25 @@
 STAKE=100
 
 #variables
-no_of_bets=0
+no_of_bets=1
 random=0
+total_amount_won=0
+total_amount_lost=0
 
-((no_of_bets++))
 echo "WELCOME TO GAMBLER GAME"
-random=$(($RANDOM%2))
-if [ $random -eq 1 ]
-then
-	echo "GAME WON BY GAMBLER"
-else
-	echo "GAME LOST BY GAMBLER"
-fi
-echo "Bet Amount = " $no_of_bets
+
+#Loops until total_amount_won and total_amount_lost is less then 50% of stake
+while [ $total_amount_won -lt $(($STAKE-50)) -a $total_amount_lost -lt $(($STAKE-50)) ]
+do
+	random=$(($RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+		total_amount_won=$(($total_amount_won+$no_of_bets))
+		echo "GAME WON BY GAMBLER"
+	else
+		total_amount_lost=$(($total_amount_lost+$no_of_bets))
+		echo "GAME LOST BY GAMBLER"
+	fi
+done
+echo "Total amount won by gambler" $total_amount_won
+echo "Total amount lost by gambler" $total_amount_lost
