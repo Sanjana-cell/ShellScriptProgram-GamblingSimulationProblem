382babb36df5aab9a2af96764c00f62f8f43877f
Sanjana K R <sanjanakr97@gmail.com>
2020-07-31 13:41:20 +0530

    Refator: modified code by calculating total amount won and lost after 20 days

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -1,28 +1,54 @@
-#!/bin/bash -x
+#!/bin/bash 
 
-#constants
 STAKE=100
+MAXIMUM_DAYS=30
+PERCENTAGE_STAKE=$((($STAKE*50)/100)) 
+MAXIMUM_AMOUNT=$(($STAKE+$PERCENTAGE_STAKE))
+MINIMUM_AMOUNT=$(($STAKE-$PERCENTAGE_STAKE))
+BET_AMOUNT=1
 
-#variables
-betAmount=1
 random=0
-stake_amount=$STAKE
+day=0
+totalAmountWon=0
+totalAmountLost=0
+numOfDaysWon=0
+numOfDaysLost=0
 
 echo "WELCOME TO GAMBLER GAME"
 
-#Loops until stake amount less then 50% of stake
-while [ $stake_amount -gt 50 -a $stake_amount -lt 150 ]
-do
-	random=$(($RANDOM%2))
-	if [ $random -eq 1 ]
+#loops till maximum days
+for (( day=0; day<=MAXIMUM_DAYS; day++))
+do 
+	stakeAmount=$STAKE
+	#Loops until stake amount is reached minimum amount or maximum amount
+	while [ $stakeAmount -gt $MINIMUM_AMOUNT -a $stakeAmount -lt $MAXIMUM_AMOUNT ]
+	do
+		random=$(($RANDOM%2))
+		if [ $random -eq 1 ]
+		then
+			#if random is 1 stake amount is incremented by 1
+			stakeAmount=$(($stakeAmount+$BET_AMOUNT))
+		else
+			#if random is 0 stake amount is decremented by 1
+			stakeAmount=$(($stakeAmount-$BET_AMOUNT))  
+		fi
+	done
+
+	if [ $stakeAmount -eq $MAXIMUM_AMOUNT ]
 	then
-		#if random is 1 stake amount is incremented by bet amount
-		stake_amount=$(($stake_amount+$betAmount))
-		echo "GAME WON BY GAMBLER"
+		((numOfDaysWon++))
 	else
-		#if random is 0 stake amount is decremented by bet amount  
-		stake_amount=$(($stake_amount-$betAmount))
-		echo "GAME LOST BY GAMBLER"
+		((numOfDaysLost++))
 	fi
+
+	#calculates total amount won or lost after 21 days
+	if [ $day -gt 20 ]
+	then
+		totalAmountWon=$(($numOfDaysWon*$PERCENTAGE_STAKE)) 
+		echo "Total amount won after "  $day "day" $totalAmountWon
+
+		totalAmountLost=$(($numOfDaysLost*$PERCENTAGE_STAKE))  
+		echo "Total amount Lost after " $day "day" $totalAmountLost 
+	fi 
 done
-echo "Stake amount" $stake_amount
+
